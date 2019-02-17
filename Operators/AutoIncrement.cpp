9c03157bf973a687dccbd4691ef06e365bbf5c54
Sarvesh Rajkumar <sarvesh2@illinois.edu>
2019-02-17 15:21:47 -0600

    Adding Operators

---
+++ Operators/AutoIncrement.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int num1 = 240;
+  int num2 = 40;
+  num1++; num2--;
+  cout<<"num1++ is: "<<num1<<endl;
+  cout<<"num2-- is: "<<num2;
+  return 0;
+}
